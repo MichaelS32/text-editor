7fe2c5f6fb09b9eeaefaece179cc60131f1b896f
mschneider <michaelschneider3232@gmail.com>
2022-09-11 10:00:05 -0500

    updated webpack and package.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "JATE",
+        title: "Webpack Plugins",
       }),
 
       new InjectManifest({
